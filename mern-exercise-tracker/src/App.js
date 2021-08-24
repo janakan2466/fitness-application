2b05653f90c47e3390779dfebfc159b97535c6fa
Janakan Sivaloganathan <janakan2466@gmail.com>
2021-08-24 08:42:23 -0400

    Implemented dependencies under the components directory

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
-
+import "bootstrap/dist/css/bootstrap.min.css";
+ 
 import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
@@ -8,18 +9,18 @@ import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
 function App() {
-  return (
-    <Router>
-      {/* <div className="container"> */}
+ return (
+   <Router>
+   <div className="container">
       <Navbar />
       <br/>
       <Route path="/" exact component={ExercisesList} />
       <Route path="/edit/:id" component={EditExercise} />
       <Route path="/create" component={CreateExercise} />
       <Route path="/user" component={CreateUser} />
-      {/* </div> */}
-    </Router>
-  );
+   </div>
+   </Router>
+ );
 }
-
+ 
 export default App;
--- mern-exercise-tracker/src/components/create-user.component.js
+++ mern-exercise-tracker/src/components/create-user.component.js
@@ -2,37 +2,33 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class CreateUser extends Component {
+
   constructor(props) {
     super(props);
-
     this.onChangeUsername = this.onChangeUsername.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
-
     this.state = {
       username: ''
-    }
+    };
   }
 
   onChangeUsername(e) {
     this.setState({
       username: e.target.value
-    })
+    });
   }
-
   onSubmit(e) {
     e.preventDefault();
+    const newUser = {
+      username: this.state.username,
+    };
+    console.log(newUser);
 
-    const user = {
-      username: this.state.username
-    }
-
-    console.log(user);
-
-    axios.post('http://localhost:5000/users/add', user)
+    axios.post('http://localhost:5000/users/add', newUser)
       .then(res => console.log(res.data));
-
+      
     this.setState({
-      username: ''
+      username: ''  
     })
   }
 
